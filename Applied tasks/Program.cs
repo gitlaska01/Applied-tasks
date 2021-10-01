423af55fd1435ea36eda25e929c532ff7aa9ffb8
nezna <nezna@DESKTOP-JER8SVN>
2021-10-01 14:03:21 +0300

    Add project files.

---
+++ Applied tasks/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Applied_tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            decimal cost = decimal.Parse(Console.ReadLine());
+            Console.WriteLine(cost * 0.8m);
+            Console.WriteLine(cost * 0.2m);
+            Console.WriteLine(MathRound(cost, 0.8m));
+            Console.WriteLine(MathRound(cost , 0.2m));
+            
+
+        }
+
+        public static int MathRound(decimal number, decimal rate)
+        {
+            int res = Convert.ToInt32(number * rate);
+            if ((((number*rate-res))*10) % 5 == 0 && number * rate - res!=0)
+            {
+                return (Convert.ToInt32(number * rate)+1);
+            }
+            else
+            {   
+                return (Convert.ToInt32((number * rate) ));
+            }
+        }
+
+
+
+
+
+        
+
+        }
+       
+        
+        
+
+        }
+    
+    
+
